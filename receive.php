964e033dffe1fa0a77d383a105b77b8348c0509e
Piotr Lipski <piotr.lipski@bbc.co.uk>
2016-06-09 14:05:50 +0100

    Improve receive functionality

--- receive.php
+++ receive.php
@@ -2,19 +2,34 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-$channel = $connection->channel();
+function receiveMessage($queue)
+{
+    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $channel    = $connection->channel();
 
-$channel->queue_declare('register', false, false, false, false);
+    $channel->queue_declare($queue, false, false, false, false);
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+    echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
-$callback = function($msg) {
-    echo " [x] Received ", $msg->body, "\n";
-};
+    $callback = function ($msg) {
+        echo " [x] Received ", $msg->body, "\n";
+    };
 
-$channel->basic_consume('register', '', false, true, false, false, $callback);
+    $channel->basic_consume($queue, '', false, true, false, false, $callback);
 
-while(count($channel->callbacks)) {
-    $channel->wait();
+    while (count($channel->callbacks)) {
+        $channel->wait();
+    }
 }
+
+$queue = $argv[1];
+
+switch ($queue) {
+    case 'register':
+    case 'userCreation' :
+        break;
+    default:
+        $queue = 'register';
+
+}
+receiveMessage($queue);
