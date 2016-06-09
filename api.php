28ebf09e6b92e10a86174a94fb503c48dce8a7b2
Piotr Lipski <piotr.lipski@bbc.co.uk>
2016-06-09 12:28:38 +0100

    ...

--- api.php
+++ api.php
@@ -4,17 +4,17 @@ require_once __DIR__ . '/names.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-function sendMessage($message = 'Hello World!')
+function sendMessage($message = 'Hello World!', $queue)
 {
     $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
     $channel    = $connection->channel();
 
-    $channel->queue_declare('register', false, false, false, false);
+    $channel->queue_declare($queue, false, false, false, false);
 
     $msg = new AMQPMessage($message);
-    $channel->basic_publish($msg, '', 'hello');
+    $channel->basic_publish($msg, '', $queue);
 
-    echo " [x] Sent 'Hello World!'\n";
+    echo "\n [x] Sent $message\n\n";
 
     $channel->close();
     $connection->close();
@@ -26,8 +26,8 @@ function sendRegisterMEssage()
     $data    = array("name" => getRandName());
     $event   = array("id" => md5(rand()), "timestamp" => time(), "event" => "registerUser", "data" => $data);
     $message = json_encode($event);
-    echo $message;
-    sendMessage($message);
+    $queue   = 'register';
+    sendMessage($message, $queue);
 }
 
 function sendListUsersMessage()
@@ -35,10 +35,12 @@ function sendListUsersMessage()
     $data    = array();
     $event   = array("id" => md5(rand()), "timestamp" => time(), "event" => "listUsers", "data" => $data);
     $message = json_encode($event);
-    sendMessage($message);
+    $queue   = 'listUsers';
+    sendMessage($message, $queue);
 }
 
+sleep(1);
 sendRegisterMEssage();
-sleep(2);
-sendListUsersMessage();
+//sleep(1);
+//sendListUsersMessage();
 
